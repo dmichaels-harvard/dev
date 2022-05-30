94d4627bb06f753d18f0ae267723d7857fa651bf
David Michaels <dmichaels@gmail.com>
2022-05-30 17:23:17 -0400

    dev

--- scripts/aws-secrets.py
+++ scripts/aws-secrets.py
@@ -4,6 +4,9 @@
 #
 # usage: aws-secrets [--secret secret-name-pattern] [--secrets] [--show]
 #
+# If --name with a simple pattern is given then limit secrets to those
+# whose name contains the specified simple pattern (case-insensitive).
+
 # If --secrets is given then the secret keys/values are printed for each secret.
 # Secret values with names that look senstive ("secret" or "password" etc) are obfuscated.
 #
@@ -18,6 +21,7 @@ import json
 import argparse
 
 args_parser = argparse.ArgumentParser()
+args_parser.add_argument("--name", type=str, required=False)
 args_parser.add_argument("--secret", type=str, required=False)
 args_parser.add_argument("--secrets", action="store_true", required=False)
 args_parser.add_argument("--show", action="store_true", required=False)
@@ -30,18 +34,27 @@ def should_obfuscate_secret(key: str) -> bool:
     return x
 
 def obfuscate(value: str) -> str:
-    return value[0:1] + "*******"
+    return value[0:1] + "*******" if value is not None and len(value) > 0 else ""
 
 def print_aws_secrets():
-    print("AWS Secrets")
+
+    print("AWS Secrets", end = "")
+    if args.name:
+        print(" / name containing: " + args.name, end = "")
+    if args.secret:
+        print(" / secret keys containing: " + args.secret, end = "")
+    print()
+
     c4 = boto3.client('secretsmanager')
-    for secret in sorted(c4.list_secrets()["SecretList"], key=lambda key: key["Name"]):
+    for secret in sorted(c4.list_secrets()["SecretList"], key=lambda key: key["Name"].lower()):
         secret_name = secret["Name"]
+        if args.name and not args.name.lower() in secret_name.lower():
+            continue
         print(secret_name)
         if args.secret or args.secrets:
             secret_value = c4.get_secret_value(SecretId=secret_name)
             secret_value_json = json.loads(secret_value["SecretString"])
-            for secret_key in sorted(secret_value_json.keys()):
+            for secret_key in sorted(secret_value_json.keys(), key=lambda key: key.lower()):
                 if args.secret and not args.secret.lower() in secret_key.lower():
                     continue
                 secret_value = secret_value_json[secret_key]
@@ -50,4 +63,5 @@ def print_aws_secrets():
                                   and should_obfuscate_secret(secret_key) else secret_value
                 print(f"- {secret_key}: {secret_value}")
 
-print_aws_secrets()
+if __name__ == "__main__":
+    print_aws_secrets()
--- scripts/aws-stacks.py
+++ scripts/aws-stacks.py
@@ -2,15 +2,15 @@
 # Simple script to view AWS stacks and related info.
 # By default prints all stacks, and optionally all associated Outputs key/values.
 #
-# usage: aws-stacks [--stack stack-name-pattern] [--output output-key-pattern ] [--outputs] [--exports]
+# usage: aws-stacks [--name stack-name-pattern] [--output output-key-pattern ] [--outputs] [--exports]
+#
+# If --name with a simple pattern is given then limit stacks to those
+# whose stack name contains the specified simple pattern (case-insensitive).
 #
 # If --outputs is given then prints all outputs key/values for each stack.
 #
 # If --output with a simple pattern is given then limit outputs key/values
 # to those whose key contains the specified simple pattern (case-insensitive).
-#
-# If --stack with a simple pattern is given then limit stacks to those
-# whose stack name contins the specified simple pattern (case-insensitive).
 #
 # If --exports is given then include export name with the outputs keys/values.
 # --------------------------------------------------------------------------------------------------
@@ -19,7 +19,7 @@ import boto3
 import argparse
 
 args_parser = argparse.ArgumentParser()
-args_parser.add_argument("--stack", type=str, required=False)
+args_parser.add_argument("--name", type=str, required=False)
 args_parser.add_argument("--output", type=str, required=False)
 args_parser.add_argument("--outputs", action="store_true", required=False)
 args_parser.add_argument("--exports", action="store_true", required=False)
@@ -28,8 +28,8 @@ args = args_parser.parse_args()
 def print_aws_stacks():
 
     print("AWS Stacks", end = "")
-    if args.stack:
-        print(" / stack name containing: " + args.stack, end = "")
+    if args.name:
+        print(" / name containing: " + args.name, end = "")
     if args.output:
         print(" / output keys containing: " + args.output, end = "")
     print()
@@ -37,7 +37,7 @@ def print_aws_stacks():
     c4 = boto3.client('cloudformation')
     for stack in c4.describe_stacks()["Stacks"]:
         stack_name = stack["StackName"]
-        if args.stack and not args.stack.lower() in stack_name.lower():
+        if args.name and not args.name.lower() in stack_name.lower():
             continue
         stack_updated = stack["LastUpdatedTime"]
         print("%-15s (updated: %s)" % (stack_name, stack_updated.astimezone().strftime("%Y:%m:%d %H:%M:%S")))
@@ -53,12 +53,14 @@ def print_aws_stacks():
                 if args.exports and stack_output_export_name:
                     print("   %s (export name)" % (stack_output_export_name))
 
-print_aws_stacks()
+if __name__ == "__main__":
+    print_aws_stacks()
 
 
 
 
 
+#JUNK...
 #c4 = boto3.resource('cloudformation')
 #stacks = c4.meta.client.list_stacks()
 #for stack in stacks:
