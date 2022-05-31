e77d0c01824f7ed086c92e6baef3823f1cdf1081
David Michaels <dmichaels@gmail.com>
2022-05-31 10:53:32 -0400

    dev

---
+++ scripts/aws-stacks-template.py
@@ -0,0 +1,36 @@
+# --------------------------------------------------------------------------------------------------
+# Simple script to view AWS stack templates.
+#
+# usage: aws-stacks-template stack-name
+# --------------------------------------------------------------------------------------------------
+
+import argparse
+import boto3
+import json
+import sys
+import yaml
+from collections import OrderedDict
+
+def print_aws_stack_template(stack_name):
+
+    c4 = boto3.client('cloudformation')
+    stack_template = c4.get_template(StackName=stack_name)
+    stack_template_body = stack_template["TemplateBody"]
+    if isinstance(stack_template_body, OrderedDict):
+        #
+        # For some reason for our AWS stack c4-foursight-cgap-supertest-stack
+        # in particular, we get back an OrderedDict, which we print as JSON.
+        #
+        stack_template_json = json.dumps(stack_template_body, default=str, indent=2)
+        print(stack_template_json)
+        pass
+    else:
+        #
+        # For other AWS stacks like c4-datastore-cgap-supertest-stack,
+        # we get a simple string containing the YAML for the template.
+        #
+        print(stack_template_body)
+
+if __name__ == "__main__":
+    stack_name = sys.argv[1]
+    print_aws_stack_template(stack_name)
--- scripts/aws-stacks.py
+++ scripts/aws-stacks.py
@@ -2,36 +2,79 @@
 # Simple script to view AWS stacks and related info.
 # By default prints all stacks, and optionally all associated Outputs key/values.
 #
-# usage: aws-stacks [--name stack-name-pattern] [--output output-key-pattern ] [--outputs] [--exports]
+# usage: aws-stacks [--name stack-name-pattern]
+#                   {
+#                   | [ --outputs | --output output-key-pattern]
+#                   | [ --resources | --resource resource-name-pattern ]
+#                   | [ --parameters | -parameter  parameter-name-pattern ]
+#                   }
+#                   [--verbose]
 #
 # If --name with a simple pattern is given then limit stacks to those
-# whose stack name contains the specified simple pattern (case-insensitive).
+# whose stack names contains the specified simple pattern (case-insensitive).
 #
 # If --outputs is given then prints all outputs key/values for each stack.
 #
 # If --output with a simple pattern is given then limit outputs key/values
-# to those whose key contains the specified simple pattern (case-insensitive).
+# to those whose keys contains the specified simple pattern (case-insensitive).
 #
-# If --exports is given then include export name with the outputs keys/values.
+# If --resource with a simple pattern is given then limit resources names/types
+# to those whose names contains the specified simple pattern (case-insensitive).
+#
+# If --resources is given then prints all resources names/types for each stack.
+#
+# If --parameters is given then prints all parameters key/values for each stack.
+#
+# If --parameter with a simple pattern is given then limit parameters key/values
+# to those whose keys contains the specified simple pattern (case-insensitive).
+#
+# Only one of --output(s) or --resource(s) or --parameter(s) may be specified.
+#
+# If --verbose is given then include export name with the outputs keys/values.
 # --------------------------------------------------------------------------------------------------
 
-import boto3
 import argparse
+import boto3
+import json
 
 args_parser = argparse.ArgumentParser()
 args_parser.add_argument("--name", type=str, required=False)
-args_parser.add_argument("--output", type=str, required=False)
 args_parser.add_argument("--outputs", action="store_true", required=False)
-args_parser.add_argument("--exports", action="store_true", required=False)
+args_parser.add_argument("--output", type=str, required=False)
+args_parser.add_argument("--verbose", action="store_true", required=False)
+args_parser.add_argument("--resources", action="store_true", required=False)
+args_parser.add_argument("--resource", type=str, required=False)
+args_parser.add_argument("--parameters", action="store_true", required=False)
+args_parser.add_argument("--parameter", type=str, required=False)
 args = args_parser.parse_args()
+
+description = ""
+mutually_exclusive_options_count = 0
+if args.outputs or args.output:
+    description = " with Outputs"
+    mutually_exclusive_options_count += 1
+if args.resources or args.resource:
+    description = " with Resources"
+    mutually_exclusive_options_count += 1
+if args.parameters or args.parameter:
+    description = " with Parameters"
+    mutually_exclusive_options_count += 1
+
+if mutually_exclusive_options_count > 1:
+    print("Must specify only one of: --output(s) --resource(s) --parameter(s)")
+    exit(1)
 
 def print_aws_stacks():
 
-    print("AWS Stacks", end = "")
+    print(f"AWS Stacks{description}", end = "")
     if args.name:
         print(" / name containing: " + args.name, end = "")
     if args.output:
         print(" / output keys containing: " + args.output, end = "")
+    if args.resource:
+        print(" / resource name containing: " + args.resource, end = "")
+    if args.parameter:
+        print(" / parameter name containing: " + args.parameter, end = "")
     print()
 
     c4 = boto3.client('cloudformation')
@@ -41,17 +84,36 @@ def print_aws_stacks():
             continue
         stack_updated = stack["LastUpdatedTime"]
         print("%-15s (updated: %s)" % (stack_name, stack_updated.astimezone().strftime("%Y:%m:%d %H:%M:%S")))
-        if args.output or args.outputs:
+        if args.outputs or args.output:
             for stack_output in sorted(stack["Outputs"], key=lambda key: key["OutputKey"]):
                 stack_output_key = stack_output["OutputKey"]
                 if args.output and not args.output.lower() in stack_output_key.lower():
                     continue
                 stack_output_value = stack_output["OutputValue"]
                 stack_output_export_name = stack_output.get("ExportName")
-                print(" - %s:" % (stack_output_key))
-                print("   %s" % (stack_output_value))
-                if args.exports and stack_output_export_name:
+                print(" - %s: %s" % (stack_output_key, stack_output_value))
+                # print(" - %s:" % (stack_output_key))
+                # print("   %s" % (stack_output_value))
+                if args.verbose and stack_output_export_name:
                     print("   %s (export name)" % (stack_output_export_name))
+        elif args.resources or args.resource:
+            for stack_resource in sorted(c4.describe_stack_resources(StackName=stack_name)["StackResources"], key=lambda key: key["LogicalResourceId"].lower()):
+                stack_resource_name = stack_resource["LogicalResourceId"]
+                stack_resource_type = stack_resource["ResourceType"]
+                if args.resource and not args.resource.lower() in stack_resource_name.lower():
+                    continue
+                print("- %s: %s" % (stack_resource_name, stack_resource_type))
+        elif args.parameters or args.parameter:
+            stack_parameters = stack.get("Parameters")
+            if stack_parameters:
+                for stack_parameter in sorted(stack_parameters, key=lambda key: key["ParameterKey"]):
+                    stack_parameter_key = stack_parameter["ParameterKey"]
+                    if args.parameter and not args.parameter.lower() in stack_parameter_key.lower():
+                        continue
+                    stack_parameter_value = stack_parameter["ParameterValue"]
+                    print(" - %s: %s" % (stack_parameter_key, stack_parameter_value))
+                    # print(" - %s:" % (stack_parameter_key))
+                    # print("   %s" % (stack_parameter_value))
 
 if __name__ == "__main__":
     print_aws_stacks()
