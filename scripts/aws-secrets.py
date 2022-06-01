5bc64808e613060c736681cfa4153a00dad0ae63
David Michaels <dmichaels@gmail.com>
2022-06-01 12:23:51 -0400

    dev

--- scripts/aws-secrets.py
+++ scripts/aws-secrets.py
@@ -1,8 +1,11 @@
 # --------------------------------------------------------------------------------------------------
 # Simple script to view AWS secrets info.
-# By default prints all secrets names.
+# By default prints just (all) the name of the secrets names available,
+# e.g. C4DatastoreCgapSupertestApplicationConfiguration. Use the --secrets
+# option to see the actualy secret keys/values for each secret name.
+# Values are obfuscated if they look like they represent true secrets.
 #
-# usage: aws-secrets [--secret secret-name-pattern] [--secrets] [--show]
+# usage: aws-secrets [--names secret-name-pattern] [--secrets] [--show]
 #
 # If --name with a simple pattern is given then limit secrets to those
 # whose name contains the specified simple pattern (case-insensitive).
@@ -16,9 +19,10 @@
 # If --show is given the prints obfuscated values in plaintext.
 # --------------------------------------------------------------------------------------------------
 
+import argparse
 import boto3
 import json
-import argparse
+import re
 
 args_parser = argparse.ArgumentParser()
 args_parser.add_argument("--name", type=str, required=False)
@@ -27,21 +31,35 @@ args_parser.add_argument("--secrets", action="store_true", required=False)
 args_parser.add_argument("--show", action="store_true", required=False)
 args = args_parser.parse_args()
 
-def should_obfuscate_secret(key: str) -> bool:
-    key = key.lower()
-    return "secret" in key or "password" in key or "passwd" in key or "crypt" in key
-
-def obfuscate(value: str) -> str:
-    return value[0:1] + "*******" if value is not None and len(value) > 0 else ""
-
 def print_aws_secrets():
+    """
+    Prints AWS secrets for the currently active AWS credential.
+    """
 
-    print("AWS Secrets", end = "")
+    SECRET_KEY_NAMES = [ ".*secret.*", ".*secrt.*", ".*password.*", ".*passwd.*", ".*crypt.*" ]
+
+    def should_obfuscate_secret(key: str) -> bool:
+        """
+        Returns True if the given key looks like it represents a secret value.
+        N.B.: Dumb implementation. Just sees if it contains "secret" or "password"
+        or "crypt" some obvious variants (case-insensitive), i.e. whatever is
+        in the secret_key_names list, which can be a regular expression.
+        Add more to secret_key_names if/when needed.
+        """
+        secret_key_names_regex = map(lambda regex: re.compile(regex, re.IGNORECASE), SECRET_KEY_NAMES)
+        return any(regex.match(key) for regex in secret_key_names_regex)
+
+    def obfuscate(value: str) -> str:
+        return value[0:1] + "*******" if value is not None and len(value) > 0 else ""
+
+    aws_access_key = boto3.Session().get_credentials().access_key
+
+    print("AWS Secrets (%s)" % aws_access_key, end = "")
     if args.name:
         print(" / name containing: " + args.name, end = "")
     if args.secret:
         print(" / secret keys containing: " + args.secret, end = "")
-    print()
+    print("")
 
     c4 = boto3.client('secretsmanager')
     for secret in sorted(c4.list_secrets()["SecretList"], key=lambda key: key["Name"].lower()):
