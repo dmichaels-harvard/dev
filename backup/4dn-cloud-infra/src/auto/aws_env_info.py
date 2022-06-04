d85cfe34720961abf7be553ba093ab729607fff8
David Michaels <dmichaels@gmail.com>
2022-06-03 23:15:39 -0400

    dev

--- backup/4dn-cloud-infra/src/auto/aws_env_info.py
+++ backup/4dn-cloud-infra/src/auto/aws_env_info.py
@@ -1,11 +1,10 @@
 import os
 import glob
 
-class AwsDirsInfo:
-
+class AwsEnvInfo:
     """
     Class to gather/dispense info about the ~/.aws_test directories
-    ala use_test_creds, i.e. what AWS credential enviroment is currently
+    ala use_test_creds, i.e. what AWS credentials enviroment is currently
     active (based on what ~/.aws_test is symlinked to), and the list
     of available environments (based on what ~/.aws_test.{ENV_NAME}
     directories actually exist).
@@ -19,47 +18,65 @@ class AwsDirsInfo:
 
     May construct with a base directory name other than ~/.aws_test if desired.
     """
+
+    # TOD)
+    # We're probably going to change this default directory name ~/.aws_test
+    # to something like ~/.aws_cgap or something; when we do we can change
+    # this, and/or of course can pass this into the AwsEnvInfo constructor.
+    #
     __DEFAULT_AWS_DIR = "~/.aws_test"
+
     def __init__(self, aws_dir = __DEFAULT_AWS_DIR):
-        aws_dir = AwsDirsInfo.__DEFAULT_AWS_DIR
         if not aws_dir:
-            aws_dir = AwsDirsInfo.__DEFAULT_AWS_DIR
-        self.aws_base_dir = os.path.basename(aws_dir)
-        self.aws_dir = os.path.expanduser(aws_dir)
+            aws_dir = AwsEnvInfo.__DEFAULT_AWS_DIR
+        self.__aws_base_dir = os.path.basename(aws_dir)
+        self.__aws_dir = os.path.expanduser(aws_dir)
+
     def __get_dirs(self):
         dirs = []
-        for dir in glob.glob(self.aws_dir + ".*"):
+        for dir in glob.glob(self.__aws_dir + ".*"):
             if os.path.isdir(dir):
                 dirs.append(dir)
         return dirs
+
     def __get_env_name_from_path(self, path: str):
         if path:
             basename = os.path.basename(path)
-            if basename.startswith(self.aws_base_dir + "."):
-                return basename[len(self.aws_base_dir) + 1:]
+            if basename.startswith(self.__aws_base_dir + "."):
+                return basename[len(self.__aws_base_dir) + 1:]
+
+    def get_base_dir(self):
+        return self.__aws_dir
+
     def get_available_envs(self):
         """
         Returns a list of available AWS environments based on what
         directories are present of the form ~/.aws_test.{ENV_NAME}.
+        Returns empty list of none found.
         """
         return [ self.__get_env_name_from_path(path) for path in self.__get_dirs() ]
+
     def get_current_env(self):
         """
         Returns the AWS environment name as represented by the ENV_NAME portion
         of the symlink target of the ~/.aws_test directory itself.
+        Returns None if not set.
         """
-        symlink_target = os.readlink(self.aws_dir) if os.path.islink(self.aws_dir) else None
+        symlink_target = os.readlink(self.__aws_dir) if os.path.islink(self.__aws_dir) else None
         return self.__get_env_name_from_path(symlink_target)
+
     def get_dir(self, env_name):
         """
         Returns a full directory path name of the form ~/.aws_test.{ENV_NAME}
         for the given :param:`env_name`.
+        :param env_name: The AWS environment name.
         """
         if env_name:
-            return self.aws_dir + "." + env_name
+            return self.__aws_dir + "." + env_name
+
 
 if __name__ == "__main__":
-    aws_dirs_info = AwsDirsInfo()
+    aws_dirs_info = AwsEnvInfo()
     aws_current_env = aws_dirs_info.get_current_env()
     if aws_current_env:
         print(f"Your current AWS (use_test_creds) environment is: {aws_current_env}")
--- backup/4dn-cloud-infra/src/auto/main.py
+++ backup/4dn-cloud-infra/src/auto/main.py
@@ -3,15 +3,16 @@
 
 import argparse
 from   enum import Enum
-import re
+import glob
 import io
 import json
-import glob
 import os
+import re
+import stat
 import subprocess
 
-from dcicutils.misc_utils import json_leaf_subst as expand_json_template
-from dcicutils.cloudformation_utils import camelize
+from  dcicutils.misc_utils import json_leaf_subst as expand_json_template
+from  dcicutils.cloudformation_utils import camelize
 from .aws_env_info import AwsEnvInfo
 
 AWS_DIR                = "~/.aws_test"
@@ -59,13 +60,13 @@ def get_fallback_deploying_iam_user():
     """
     return os.environ.get("USER")
 
-def get_fallback_identity(aws_env: str):
+def get_fallback_identity(env_name: str):
     """
     Obtains/returns the identity by simple concantentation of strings
     the same way the 4dn-cloud-infra code does it.
     TODO: Get this directly via 4dn-cloud-infra code.
     """
-    return "C4Datastore" + camelize(aws_env) + "ApplicationConfiguration"
+    return "C4Datastore" + camelize(env_name) + "ApplicationConfiguration"
 
 def expand_json_template_file(template_file: str, output_file: str, template_substitutions: dict):
     if not os.path.isfile(template_file):
@@ -89,7 +90,7 @@ def generate_s3_encrypt_key():
           probably should also replicate the openssl version checking.
     """
     s3_encrypt_key_command = "openssl enc -aes-128-cbc -k `ps -ax | md5` -P -pbkdf2 -a"
-    s3_encrypt_key_command_output = subprocess.check_output(s3_encrypt_key_command, shell=True).decode("utf-8").strip()
+    s3_encrypt_key_command_output = subprocess.check_output(s3_encrypt_key_command, shell=True).decode("utf-8")
     return re.compile("key=(.*)\n").search(s3_encrypt_key_command_output).group(1)
 
 def confirm_with_user(message: str):
@@ -114,8 +115,7 @@ def print_directory_tree(directory: str):
         contents = [os.path.join(directory, item) for item in os.listdir(directory)]
         pointers = [tee] * (len(contents) - 1) + [last]
         for pointer, path in zip(pointers, contents):
-            if os.path.islink(path): symlink = "@ -> " + os.readlink(path)
-            else: symlink = ""
+            symlink = "@ -> " + os.readlink(path) if os.path.islink(path) else ""
             yield prefix + pointer + os.path.basename(path) + symlink # + modified_time
             if os.path.isdir(path):
                 extension = branch if pointer == tee else space 
@@ -125,75 +125,67 @@ def print_directory_tree(directory: str):
 
 def main():
 
-    args_parser = argparse.ArgumentParser()
-    args_parser.add_argument("--env", type=str, required=True)
-    args_parser.add_argument("--out", type=str, default=CUSTOM_DIR, required=False)
-    args_parser.add_argument("--account", type=str, required=False)
-    args_parser.add_argument("--username", type=str, required=False)
-    args_parser.add_argument("--identity", type=str, required=False)
-    args_parser.add_argument("--s3org", type=str, required=True)
-    args_parser.add_argument("--auth0client", type=str, required=False)
-    args_parser.add_argument("--auth0secret", type=str, required=False)
-    args_parser.add_argument("--recaptchakey", type=str, required=False)
-    args_parser.add_argument("--recaptchasecret", type=str, required=False)
-    args_parser.add_argument("--debug", action="store_true", required=False)
-    args_parser.add_argument("--yes", action="store_true", required=False)
-    args = args_parser.parse_args()
-
-    aws_env = args.env
-    custom_dir = os.path.abspath(args.out)
-    account_number = args.account
-    deploying_iam_user = args.username
-    identity = args.identity
-    s3_bucket_org = args.s3org
-    auth0_client = args.auth0client
-    auth0_secret = args.auth0secret
-    re_captcha_key = args.recaptchakey
-    re_captcha_secret = args.recaptchasecret
+    argp = argparse.ArgumentParser()
+    argp.add_argument("--env",             dest='env_name', type=str, required=True)
+    argp.add_argument("--out",             dest='custom_dir_name', type=str, default=CUSTOM_DIR, required=False)
+    argp.add_argument("--account",         dest='account_number', type=str, required=False)
+    argp.add_argument("--username",        dest='deploying_iam_user', type=str, required=False)
+    argp.add_argument("--identity",        dest='identity', type=str, required=False)
+    argp.add_argument("--s3org",           dest='s3_bucket_org', type=str, required=True)
+    argp.add_argument("--auth0client",     dest='auth0_client', type=str, required=False)
+    argp.add_argument("--auth0secret",     dest='auth0_secret', type=str, required=False)
+    argp.add_argument("--recaptchakey",    dest='re_captcha_key', type=str, required=False)
+    argp.add_argument("--recaptchasecret", dest='re_captcha_secret', type=str, required=False)
+    argp.add_argument("--debug",           dest='debug', action="store_true", required=False)
+    argp.add_argument("--yes",             dest='yes', action="store_true", required=False)
+    args = argp.parse_args()
 
     if args.debug:
         print(f"Current directory: {os.getcwd()}")
         print(f"Current username: {os.environ['USER']}")
 
-    aws_env_info = AwsEnvInfo(AWS_DIR)
-    aws_current_env = aws_env_info.get_current_env()
+    # Get basic environment info.
+
+    env_info       = AwsEnvInfo(AWS_DIR)
+    current_env    = env_info.get_current_env()
+    available_envs = env_info.get_available_envs()
 
     # Make sure the AWS environment name given is good.
     # Required but just in case not set anyways, check current
     # environment, if set, and ask them if they want to use that.
     # But don't do this if --yes option given.
 
-    if not aws_env and args.yes:
-        if not aws_current_env:
+    if not args.env_name and args.yes:
+        if not current_env:
             exit_without_doing_anything("No environment specified. Use the --env option to specify this.")
         else:
-            aws_env = aws_current_env
+            args.env_name = current_env
             print(f"No environment specified. Use the --env option to specify this.")
-            print(f"Though it looks like your current environment is: {aws_current_env}")
-            if not confirm_with_user(f"Do you want to use this ({aws_current_env})?"):
+            print(f"Though it looks like your current environment is: {current_env}")
+            if not confirm_with_user(f"Do you want to use this ({current_env})?"):
                 exit_without_doing_anything()
 
     # Make sure the environment specified
     # actually exists as a ~/.aws_test.ENV_NAME directory.
 
-    aws_available_envs = aws_env_info.get_available_envs()
-    if aws_env not in aws_available_envs:
-        print(f"No environment for this name exists: {aws_env}")
-        if aws_available_envs:
+    if args.env_name not in available_envs:
+        print(f"No environment for this name exists: {args.env_name}")
+        if available_envs:
             print("Available environments:")
-            for aws_available_env in sorted(aws_available_envs):
-                print(f"- {aws_available_env} ({aws_env_info.get_dir(aws_available_env)})")
+            for aws_available_env in sorted(available_envs):
+                print(f"- {aws_available_env} ({env_info.get_dir(aws_available_env)})")
             exit_without_doing_anything("Choose on of the above environment using the --env option.")
         else:
             exit_without_doing_anything \
-               (f"No environments found at all.\nYou need to have at least one {aws_env_info.get_base_dir()}.{{ENV_NAME}} directory setup.") 
+               (f"No environments found at all.\nYou need to have at least one {env_info.get_base_dir()}.{{ENV_NAME}} directory setup.") 
 
-    aws_dir = aws_env_info.get_dir(aws_env)
-    print(f"Setting up local custom config directory for environment: {aws_env}")
+    aws_dir = env_info.get_dir(args.env_name)
+    print(f"Setting up local custom config directory for environment: {args.env_name}")
     print(f"Your AWS credentials directory: {aws_dir}")
 
     # Create the custom directory; but make sue it doesn't already exist.
 
+    custom_dir = os.path.abspath(args.custom_dir_name)
     if not custom_dir:
         exit_without_doing_anything("You must specify a custom output directory using the --out option.")
     if os.path.exists(custom_dir):
@@ -203,46 +195,47 @@ def main():
 
     # Check all the inputs.
 
-    if not account_number:
-        account_number = get_fallback_account_number(aws_env_info.get_dir(aws_env))
-        if not account_number:
+    if not args.account_number:
+        env_dir = env_info.get_dir(args.env_name)
+        args.account_number = get_fallback_account_number(env_dir)
+        if not args.account_number:
             exit_without_doing_anything("Cannot determine account number. Use the --account option.")
-    print(f"Using account number: {account_number}")
+    print(f"Using account number: {args.account_number}")
 
-    if not deploying_iam_user:
-        deploying_iam_user = get_fallback_deploying_iam_user()
-        if not deploying_iam_user:
+    if not args.deploying_iam_user:
+        args.deploying_iam_user = get_fallback_deploying_iam_user()
+        if not args.deploying_iam_user:
             exit_without_doing_anything(f"Cannot determine deploying IAM username. Use the --username option.")
-    print(f"Using deploying IAM username: {deploying_iam_user}")
+    print(f"Using deploying IAM username: {args.deploying_iam_user}")
 
-    if not identity:
-        identity = get_fallback_identity(aws_env)
-        if not identity:
+    if not args.identity:
+        args.identity = get_fallback_identity(args.env_name)
+        if not args.identity:
             exit_without_doing_anything(f"Cannot determine deploying IAM username. Use the --username option.")
-    print(f"Using identity: {identity}")
+    print(f"Using identity: {args.identity}")
 
-    if not s3_bucket_org:
+    if not args.s3_bucket_org:
         exit_without_doing_anything(f"You must specify an S3 bucket organization name. Use the --s3org option.")
-    print(f"Using S3 bucket organization name: {s3_bucket_org}")
+    print(f"Using S3 bucket organization name: {args.s3_bucket_org}")
 
-    if not auth0_client:
+    if not args.auth0_client:
         print("You must specify a Auth0 client ID using the --auth0client option.")
-        auth0_client = input("Or enter your Auth0 client ID: ").strip()
-        if not auth0_client:
+        args.auth0_client = input("Or enter your Auth0 client ID: ").strip()
+        if not args.auth0_client:
             exit_without_doing_anything(f"You must specify an Auth0 client. Use the --auth0client option.")
-    print(f"Using Auth0 client: {auth0_client}")
+    print(f"Using Auth0 client: {args.auth0_client}")
 
-    if not auth0_secret:
+    if not args.auth0_secret:
         print("You must specify a Auth0 secret using the --auth0secret option.")
-        auth0_secret = input("Or enter your Auth0 secret ID: ").strip()
-        if not auth0_secret:
+        args.auth0_secret = input("Or enter your Auth0 secret ID: ").strip()
+        if not args.auth0_secret:
             exit_without_doing_anything(f"You must specify an Auth0 secret. Use the --auth0secret option.")
-    print(f"Using Auth0 secret: {auth0_secret}")
+    print(f"Using Auth0 secret: {args.auth0_secret}")
 
-    if re_captcha_key:
-        print(f"Using reCaptchaKey: {re_captcha_key}")
-    if re_captcha_secret:
-        print(f"Using reCaptchaSecret: {re_captcha_secret}")
+    if args.re_captcha_key:
+        print(f"Using reCaptchaKey: {args.re_captcha_key}")
+    if args.re_captcha_secret:
+        print(f"Using reCaptchaSecret: {args.re_captcha_secret}")
 
     # Confirm with the user the everything looks okay.
 
@@ -269,11 +262,11 @@ def main():
 
     if not expand_json_template_file(config_template_file, config_file,
     {
-        ConfigTemplateVars.ACCOUNT_NUMBER.value:     account_number,
-        ConfigTemplateVars.DEPLOYING_IAM_USER.value: deploying_iam_user,
-        ConfigTemplateVars.IDENTITY.value:           identity,
-        ConfigTemplateVars.S3_BUCKET_ORG.value:      s3_bucket_org,
-        ConfigTemplateVars.ENCODED_ENV_NAME.value:   aws_env
+        ConfigTemplateVars.ACCOUNT_NUMBER.value:     args.account_number,
+        ConfigTemplateVars.DEPLOYING_IAM_USER.value: args.deploying_iam_user,
+        ConfigTemplateVars.IDENTITY.value:           args.identity,
+        ConfigTemplateVars.S3_BUCKET_ORG.value:      args.s3_bucket_org,
+        ConfigTemplateVars.ENCODED_ENV_NAME.value:   args.env_name
     }): print(f"Error writing: {config_file} (from: {config_template_file})")
 
     # Create the secrets.json file from the template and the inputs.
@@ -290,20 +283,20 @@ def main():
 
     if not expand_json_template_file(secrets_template_file, secrets_file,
     {
-        SecretsTemplateVars.AUTH0_CLIENT.value:      auth0_client,
-        SecretsTemplateVars.AUTH0_SECRET.value:      auth0_secret,
-        SecretsTemplateVars.RE_CAPTCHA_KEY.value:    re_captcha_key,
-        SecretsTemplateVars.RE_CAPTCHA_SECRET.value: re_captcha_secret
+        SecretsTemplateVars.AUTH0_CLIENT.value:      args.auth0_client,
+        SecretsTemplateVars.AUTH0_SECRET.value:      args.auth0_secret,
+        SecretsTemplateVars.RE_CAPTCHA_KEY.value:    args.re_captcha_key,
+        SecretsTemplateVars.RE_CAPTCHA_SECRET.value: args.re_captcha_secret
     }):
         print(f"Error writing: {secrets_file} (from: {secrets_template_file})")
 
-    # Create the symlink from custom/aws_creds to: ~/.aws_test.ENV_NAME
+    # Create the symlink from custom/aws_creds to ~/.aws_test.ENV_NAME.
 
     custom_aws_dir = os.path.abspath(os.path.join(custom_dir, CUSTOM_AWS_DIR))
     print(f"Creating symlink: {custom_aws_dir} -> {aws_dir} ")
     os.symlink(aws_dir, custom_aws_dir)
 
-    # Create the S3 encrypt key file.
+    # Create the S3 encrypt key file (with mode 400).
 
     s3_encrypt_key = generate_s3_encrypt_key()
     s3_encrypt_key_file = os.path.abspath(os.path.join(custom_aws_dir, S3_ENCRYPT_KEY_FILE))
@@ -315,6 +308,7 @@ def main():
         with io.open(s3_encrypt_key_file, "w") as s3_encrypt_key_f:
             s3_encrypt_key_f.write(s3_encrypt_key)
             s3_encrypt_key_f.write("\n")
+        os.chmod(s3_encrypt_key_file, stat.S_IRUSR)
 
     # Done. Summarize.
 
