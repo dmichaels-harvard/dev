381dfa34977f32341247dfda40bd4d60e7624599
David Michaels <dmichaels@gmail.com>
2022-06-07 10:16:39 -0400

    dev

--- backup/4dn-cloud-infra/src/auto/utils.py
+++ backup/4dn-cloud-infra/src/auto/utils.py
@@ -1,12 +1,27 @@
 # IN PROGRESS / dmichaels / 2022-06-04
+#
+# Module with miscellaneous utilities.
+#
+# Testing notes:
+# - External resources accesed by this module:
+#   - filesystem via:
+#     - glob.glob
+#     - io.open
+#     - os.listdir
+#     - os.path.basename
+#     - os.path.isdir
+#     - os.path.join
+#     - os.readlink
 
+import binascii
 import io
 import json
 import os
+import pbkdf2
 import secrets 
 import string 
+from   dcicutils.misc_utils import json_leaf_subst as expand_json_template
 
-from  dcicutils.misc_utils import json_leaf_subst as expand_json_template
 
 def expand_json_template_file(template_file: str, output_file: str, template_substitutions: dict):
     """
@@ -24,34 +39,54 @@ def expand_json_template_file(template_file: str, output_file: str, template_sub
         json.dump(expanded_template_json, output_f, indent=2)
         output_f.write("\n")
 
-def generate_s3_encrypt_key(length = 32):
+def generate_s3_encrypt_key():
+    """ Generate a cryptographically secure encryption key suitable for AWS S3 encryption.
+        References:
+        https://cryptobook.nakov.com/symmetric-key-ciphers/aes-encrypt-decrypt-examples#password-to-key-derivation
+        https://docs.python.org/3/library/secrets.html#recipes-and-best-practices
     """
-    Returns a value suitable for an S3 encryption key.
-    We use the cryptographically secure Python 'secrets' module.
-    See: https://docs.python.org/3/library/secrets.html
-    """
-    return "".join(secrets.choice(string.ascii_letters + string.digits) for i in range(length))
+    def generate_password():
+        SYSTEM_WORDS_DICTIONARY_FILE = '/usr/share/dict/words'
+        password = ""
+        if os.path.isfile(SYSTEM_WORDS_DICTIONARY_FILE):
+            try:
+                with open(system_words_file) as system_words_f:
+                    words = [word.strip() for word in system_words_f]
+                    password = "".join(secrets.choice(words) for i in range(4))
+            except Exception as e:
+                pass
+        #
+        # As fallback, and in any case, tack on a random token.
+        #
+        return password + secrets.token_hex(16)
+    password = generate_password()
+    password_salt = os.urandom(16)
+    s3_encrypt_key = pbkdf2.PBKDF2(password, password_salt).read(16)
+    s3_encrypt_key = binascii.hexlify(s3_encrypt_key).decode('utf-8')
+    return s3_encrypt_key
 
-    # TODO - OLD
+    # Second try:
+    # return secrets.token_hex(16)
+
+    # First try:
     # Replicating exactly the method used in scripts/create_s3_encrypt_key but
     # should we rather modify that script and call out to it? And if we do do
     # it here then may need to also replicate the openssl version checking.
-    #
     # s3_encrypt_key_command = "openssl enc -aes-128-cbc -k `ps -ax | md5` -P -pbkdf2 -a"
     # s3_encrypt_key_command_output = subprocess.check_output(s3_encrypt_key_command, shell=True).decode("utf-8").strip()
     # return re.compile("key=(.*)\n").search(s3_encrypt_key_command_output).group(1)
 
+def obfuscate(value: str):
+    return value[0] + "********" if isinstance(value, str) else ""
+
 def confirm_with_user(message: str):
     """
     Prompts the user with the given message and asks for yes or no.
-    Returns True if 'yes' (exactly, case-insensitive) otherwise False.
+    Returns True if 'yes' (exactly, trimmed, case-insensitive) otherwise False.
     """
-    input_answer = input(message + " (yes|no) ").strip().lower()
-    if input_answer == "yes":
-        return True
-    return False
+    return input(message + " (yes|no) ").strip().lower() == "yes"
 
-def exit_with_no_action(message: str = "", status: int = 1):
+def exit_with_no_action(message: str = "", status: int = 0):
     if message:
         print(message)
     print("Exiting without doing anything.")
@@ -62,7 +97,7 @@ def print_directory_tree(directory: str):
     Prints the given directory as a tree. Taken/adapted from:
     https://stackoverflow.com/questions/9727673/list-directory-tree-structure-in-python
     """
-    def tree_generator(directory, prefix: str = ''):
+    def tree_generator(directory: str, prefix: str = ''):
         space = '    ' ; branch = '│   ' ; tee = '├── ' ; last = '└── '
         contents = [os.path.join(directory, item) for item in sorted(os.listdir(directory))]
         pointers = [tee] * (len(contents) - 1) + [last]
