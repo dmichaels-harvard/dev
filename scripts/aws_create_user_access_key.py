083d13e2b27c4b62b96c79dbfb4a1a5854631f74
David Michaels <dmichaels@gmail.com>
2022-06-17 14:27:49 -0400

    dev

--- scripts/aws_create_user_access_key.py
+++ scripts/aws_create_user_access_key.py
@@ -15,7 +15,11 @@ def create_aws_user_access_key(name: str,
 
     iam = boto3.resource('iam', aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name=region)
     user = [user for user in iam.users.all() if user.name == name]
-    if not user or len(user) != 1:
+    if not user or len(user) <= 0:
+        print("AWS user not found: {name}")
+        return False
+    if len(user) > 1:
+        print("Too many AWS users found for: {name}")
         return False
     user = user[0]
     sts = boto3.client('sts', aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name=region)
@@ -26,6 +30,7 @@ def create_aws_user_access_key(name: str,
         key_pair = user.create_access_key_pair()
         print(f"AWS Access Key ID ({user.name}): {key_pair.id}")
         print(f"AWS Secret Access Key ({user.name}): {key_pair.secret}")
+    return True
 
 
 def main():
