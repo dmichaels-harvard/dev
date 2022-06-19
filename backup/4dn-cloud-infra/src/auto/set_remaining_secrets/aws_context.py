385aa468fb66074695e141bb7f47cb480bd71ce6
David Michaels <dmichaels@gmail.com>
2022-06-19 12:03:09 -0400

    dev

--- backup/4dn-cloud-infra/src/auto/set_remaining_secrets/aws_context.py
+++ backup/4dn-cloud-infra/src/auto/set_remaining_secrets/aws_context.py
@@ -8,7 +8,7 @@ class AwsContext:
     I.e. neither on the AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY environment variables, nor
     on the ~/.aws/credentials file (or the AWS_SHARED_CREDENTIALS_FILE environment variable).
     A specific path to the ~/.aws credentials directory must be specified, which will setup
-    the context to refer to the 'credentials' and/or 'config' file(s) there; or, specific
+    the context to refer to the 'credentials' and 'config' file(s) there; or, specific
     AWS access key ID and associated secret access key values need to be specified; the
     latter taking precedence over the former. Usage looks like this:
 
@@ -17,6 +17,7 @@ class AwsContext:
             do_something_with_boto3()
             reference_context_values(aws.access_key_id, aws.secret_access_key, aws.account_number, aws.default_region)
     """
+
     def __init__(self, custom_aws_creds_dir: str, access_key: str = None, secret_key: str = None, region: str = None):
         # This reset of the boto3.DEFAULT_SESSION is to workaround an odd problem with boto3
         # caching a default session even bad or non-existent credentials. It was manifest due
@@ -34,22 +35,29 @@ class AwsContext:
 
     @contextlib.contextmanager
     def establish_credentials(self):
-        # Credentials for this process need to come from custom/aws_creds directory,
-        # or be directly passed in. We don't want to come from environment.
+
+        def save_and_unset_environment_variables(environment_variables: list) -> list:
+            saved_environment_variables = {}
+            for environment_variable in environment_variables:
+                saved_environment_variables[environment_variable] = os.environ.pop(environment_variable, None)
+                if environment_variable.endswith("_FILE"):
+                    os.environ[environment_variable] = "/dev/null"
+            return saved_environment_variables
+
+        def restore_environment_variables(saved_environment_variables: list) -> None:
+            for saved_environment_variable, saved_environment_variable_value in saved_environment_variables.items():
+                if saved_environment_variable_value is not None:
+                    os.environ[saved_environment_variable] = saved_environment_variable_value
+                else:
+                    os.environ.pop(saved_environment_variable, None)
+
+        saved_environment_variables = save_and_unset_environment_variables([ "AWS_ACCESS_KEY_ID",
+                                                                             "AWS_SECRET_ACCESS_KEY",
+                                                                             "AWS_SHARED_CREDENTIALS_FILE",
+                                                                             "AWS_CONFIG_FILE",
+                                                                             "AWS_DEFAULT_REGION" ])
+        boto3.DEFAULT_SESSION = None
         try:
-            boto3.DEFAULT_SESSION = None
-            save_AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-            save_AWS_SECRET_ACCESS_KEY= os.environ.get("AWS_SECRET_ACCESS_KEY")
-            save_AWS_SHARED_CREDENTIALS_FILE = os.environ.get("AWS_SHARED_CREDENTIALS_FILE")
-            save_AWS_CONFIG_FILE = os.environ.get("AWS_CONFIG_FILE")
-            save_AWS_DEFAULT_REGION = os.environ.get("AWS_DEFAULT_REGION")
-
-            os.environ.pop("AWS_ACCESS_KEY_ID", None)
-            os.environ.pop("AWS_SECRET_ACCESS_KEY", None)
-            os.environ.pop("AWS_SHARED_CREDENTIALS_FILE", "/dev/null")
-            os.environ.pop("AWS_CONFIG_FILE", None)
-            os.environ.pop("AWS_DEFAULT_REGION", None)
-
             if self._aws_access_key_id and self._aws_secret_access_key:
                 os.environ["AWS_ACCESS_KEY_ID"] = self._aws_access_key_id
                 os.environ["AWS_SECRET_ACCESS_KEY"] = self._aws_secret_access_key
@@ -75,26 +83,7 @@ class AwsContext:
         except Exception as e:
             print(f"EXCEPTION! {str(e)}")
         finally:
-            if save_AWS_ACCESS_KEY_ID:
-                os.environ["AWS_ACCESS_KEY_ID"] = save_AWS_ACCESS_KEY_ID
-            else:
-                os.environ.pop("AWS_ACCESS_KEY_ID", None)
-            if save_AWS_SECRET_ACCESS_KEY:
-                os.environ["AWS_SECRET_ACCESS_KEY"] = save_AWS_SECRET_ACCESS_KEY
-            else:
-                os.environ.pop("AWS_ACCESS_KEY_ID", None)
-            if save_AWS_SHARED_CREDENTIALS_FILE:
-                os.environ["AWS_SHARED_CREDENTIALS_FILE"] = save_AWS_SHARED_CREDENTIALS_FILE
-            else:
-                os.environ.pop("AWS_SHARED_CREDENTIALS_FILE", None)
-            if save_AWS_CONFIG_FILE:
-                os.environ["AWS_CONFIG_FILE"] = save_AWS_CONFIG_FILE
-            else:
-                os.environ.pop("AWS_CONFIG_FILE", None)
-            if save_AWS_DEFAULT_REGION:
-                os.environ["AWS_DEFAULT_REGION"] = save_AWS_DEFAULT_REGION
-            else:
-                os.environ.pop("AWS_DEFAULT_REGION", None)
+            restore_environment_variables(saved_environment_variables)
             self.access_key_id = None
             self.secret_access_key = None
             self.default_region = None
