27ab801b4371a249c4e65c8a0fa49e024879c3e8
David Michaels <dmichaels@gmail.com>
2022-06-20 17:44:27 -0400

    dev

--- scripts/aws_kms.py
+++ scripts/aws_kms.py
@@ -1,5 +1,6 @@
 import argparse
 import boto3
+import json
 from aws_utils import (obfuscate, should_obfuscate, validate_aws)
 
 
@@ -9,11 +10,12 @@ def get_kms_keys(customer: bool, access_key: str = None, secret_key: str = None,
     for key in kms.list_keys()["Keys"]:
         key_id = key["KeyId"]
         key_description = kms.describe_key(KeyId=key_id)
-        # print(key_description)
         key_metadata = key_description["KeyMetadata"]
         key_manager = key_metadata["KeyManager"]
         if not customer or key_manager == "CUSTOMER":
-            result_keys.append(key_id)
+            key_policy = kms.get_key_policy(KeyId=key_id, PolicyName="default")
+            key_creation_date = key_metadata["CreationDate"]
+            result_keys.append((key_id, key_policy, key_creation_date))
     return sorted(result_keys, key=lambda key: key)
 
 
@@ -23,16 +25,19 @@ def main():
     args_parser.add_argument("--access-key", type=str, required=False)
     args_parser.add_argument("--secret-key", type=str, required=False)
     args_parser.add_argument("--region", type=str, required=False)
+    args_parser.add_argument("--policy", action="store_true", required=False)
     args = args_parser.parse_args()
 
     print(f"AWS KMS Utility{' | customer-managed' if args.customer else ''}")
 
-
     access_key, secret_key, region = validate_aws(args.access_key, args.secret_key, args.region)
 
     keys = get_kms_keys(args.customer, access_key=access_key, secret_key=secret_key, region=region)
-    for key in keys:
-        print(key)
+    for key_id, key_policy, key_created in keys:
+        print(f"Key ID: {key_id} (created: {key_created.astimezone().strftime('%Y-%m-%d %H:%M:%S')})")
+        if args.policy:
+            print(json.dumps(json.loads(key_policy["Policy"]), indent=2))
+            #print(json.dumps(json.loads(key_policy), indent=2))
 
 
 if __name__ == "__main__":
