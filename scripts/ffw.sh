07bbdd6e7331e2671c0dfc4137bb15f16bd4ce89
David Michaels <dmichaels@gmail.com>
2022-07-11 08:30:21 -0400

    dev

--- scripts/ffw.sh
+++ scripts/ffw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # --------------------------------------------------------------------------------------------------
 # My simple common find tool. This one is case-insensitive and as-word.
-# usage: ff.sh search_string [file_pattern] [--dryrun] [--exclude directory_names_to_exclude...]
+# For usage see: f.sh
 # --------------------------------------------------------------------------------------------------
 
 ~/bin/f.sh --grep 'grep -i -w' "$@"
